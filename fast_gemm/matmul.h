519a9056ac1d2fce5ee7e4550582291968652e67
burakyiImaz <132089341+burakyiImaz@users.noreply.github.com>
2026-03-22 18:07:54 +0000

    matmul .h updated

--- fast_gemm/matmul.h
+++ fast_gemm/matmul.h
@@ -7,6 +7,23 @@ class FastMatmul
 
 public:
 
-static void multiply(Matrix& A,Matrix& B,Matrix& C);
+static void multiply(const Matrix& A, const Matrix& B, Matrix& C);
+
+static void multiply_raw(
+	const double* A,
+	const double* B,
+	double* C,
+	int M,
+	int K,
+	int N,
+	int lda,
+	int ldb,
+	int ldc
+);
+
+static void set_blas_min_dim(int min_dim);
+static int get_blas_min_dim();
+static void set_blas_min_mnk(long long min_mnk);
+static long long get_blas_min_mnk();
 
 };
